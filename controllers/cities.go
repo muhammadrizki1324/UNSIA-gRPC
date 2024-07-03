e7c83c3f4c1ca0b94b8d9d109c8f7f709dd84331
muhammadrizki1324 <nexton021@gmail.com>
2024-07-03 21:58:51 +0700

    Add Unary Stream Get Cities

--- controllers/cities.go
+++ controllers/cities.go
@@ -5,6 +5,9 @@ import (
 	"database/sql"
 	"unsia/models"
 	"unsia/pb/cities"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // City struct
@@ -17,6 +20,33 @@ func (s *City) GetCity(ctx context.Context, in *cities.Id) (*cities.City, error)
 	var cityModel models.City
 	err := cityModel.Get(ctx, s.DB, in)	
 	return &cityModel.Pb, err
+}
+// Stream City
+func (s *City) GetCities(in *cities.EmptyMessage, stream cities.CitiesService_GetCitiesServer) error {
+	var query = "SELECT id, name FROM cities"
+	row, err := s.DB.Query(query)
+	if err != nil {
+		return err
+	}
+	defer row.Close()
+
+	for row.Next() {
+		var city cities.City
+		err = row.Scan(&city.Id, &city.Name)
+		if err != nil {
+			return err
+		}
+		res := &cities.CitiesStream{
+			City: &city,
+		}
+     
+		err = stream.Send(res)
+		if err != nil {
+			return status.Errorf(codes.Unknown, "cannot send stream response: %v", err)
+		}
+	}
+
+	return nil
 }
 //Create City
 func (s *City) Create(ctx context.Context, in *cities.CityInput) (*cities.City, error){
