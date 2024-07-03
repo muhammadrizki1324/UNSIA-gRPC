0eee64bbd2c7786c079d063cc768d0e78a2b2495
muhammadrizki1324 <nexton021@gmail.com>
2024-07-03 21:04:50 +0700

    Add Connection Postgres

---
+++ pkg/database/postgres.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+)
+
+func OpenDB() (*sql.DB, error) {
+	var db *sql.DB
+	port, err := strconv.Atoi("5432")
+	if err != nil {
+		return db, err
+	}
+
+	return sql.Open("postgres",
+		fmt.Sprintf(
+			"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			"localhost", port, "postgres", "postgres", "unsia_go",
+		),
+
+	)
+
+}
